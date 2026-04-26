39d3590ba650ffd89f0887e53d100c79d2e77197
Paolo Paci <paolopci@yahoo.it>
2026-04-26 10:57:36 +0200

    Add persistent shopping cart

---
+++ Data/Entities/CartEntity.cs
@@ -0,0 +1,12 @@
+namespace DashboardOrders.Data.Entities;
+
+public class CartEntity
+{
+    public int Id { get; set; }
+    public string CustomerEmail { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime ExpiresAt { get; set; }
+
+    public ICollection<CartItemEntity> Items { get; set; } = new List<CartItemEntity>();
+}
---
+++ Data/Entities/CartItemEntity.cs
@@ -0,0 +1,14 @@
+namespace DashboardOrders.Data.Entities;
+
+public class CartItemEntity
+{
+    public int Id { get; set; }
+    public int CartId { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    public CartEntity Cart { get; set; } = null!;
+    public ProductEntity Product { get; set; } = null!;
+}
